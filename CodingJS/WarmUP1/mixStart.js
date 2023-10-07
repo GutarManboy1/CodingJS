d3fbc6cf275490748fd3b115507ec007ae4f6cfa
Glenn Torrens <122960936+GutarManboy1@users.noreply.github.com>
2023-10-07 22:12:32 +0900

    Add files via upload

---
+++ CodingJS/WarmUP1/mixStart.js
@@ -0,0 +1,14 @@
+// Return true if the given string begins with 'mix', except the 'm' can be anything, so 'pix', '9ix' .. all count.
+
+// Examples
+
+// mixStart('mix snacks') → true
+// mixStart('pix snacks') → true
+// mixStart('piz snacks') → false
+
+
+function mixStart(str){
+    return str.match(/ix/)? true:false
+  }
+
+    //this was pretty easy
