74f711a5dcff2b2cb59c6625b1a4006a0146ba32
Glenn Torrens <122960936+GutarManboy1@users.noreply.github.com>
2023-10-12 23:53:12 +0900

    Add files via upload

---
+++ CodingJS/WarmUP1/stringTimes.js
@@ -0,0 +1,12 @@
+// Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+// Examples
+
+// stringTimes('Hi', 2) → HiHi
+// stringTimes('Hi', 3) → HiHiHi
+// stringTimes('Hi', 1) → Hi
+
+function stringTimes(str, n){
+    return str.repeat(n)}
+
+    //this was so easy!
