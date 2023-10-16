ad11ed0d1017284a296821013604c4b0b3f42fbd
Glenn Torrens <122960936+GutarManboy1@users.noreply.github.com>
2023-10-16 22:34:33 +0900

    Add files via upload

---
+++ CodingJS/WarmUP1/extraFront.js
@@ -0,0 +1,11 @@
+// Given a string, return a new string made of 3 copies of the first 2 chars of the original string. The string may be any length. If there are fewer than 2 chars, use whatever is there.
+
+// Examples
+
+// extraFront('Hello') → HeHeHe
+// extraFront('ab') → ababab
+// extraFront('H') → HHH
+
+function extraFront(str){
+    return str.substring(0,2).repeat(3)
+  }
