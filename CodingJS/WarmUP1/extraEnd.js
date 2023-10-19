c5486b3779c3b4283efb2d3e0d64e80aaa658286
Glenn Torrens <122960936+GutarManboy1@users.noreply.github.com>
2023-10-19 17:04:32 +0900

    Add files via upload

---
+++ CodingJS/WarmUP1/extraEnd.js
@@ -0,0 +1,13 @@
+// Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+
+// Examples
+
+// extraEnd('Hello') → lololo
+// extraEnd('ab') → ababab
+// extraEnd('Hi') → HiHiHi
+
+function extraEnd(str){
+    return str.slice(-2).repeat(3)
+  }
+
+  //pretty easy
