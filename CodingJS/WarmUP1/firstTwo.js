0958edf178cb1db8fa74a2d8344b4dea20a5e9f4
Glenn Torrens <122960936+GutarManboy1@users.noreply.github.com>
2023-10-19 17:10:39 +0900

    Add files via upload

---
+++ CodingJS/WarmUP1/firstTwo.js
@@ -0,0 +1,17 @@
+// Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "". Note that str.length() returns the length of a string.
+
+// Examples
+
+// firstTwo('Hello') → He
+// firstTwo('abcdefg') → ab
+// firstTwo('ab') → ab
+
+function firstTwo(str){
+    if (str.length <=2){
+    return str
+    }else{
+    return (str.slice(0,2))
+    }
+  }
+
+  //Am i actually getting better at this?! This was easy too.
