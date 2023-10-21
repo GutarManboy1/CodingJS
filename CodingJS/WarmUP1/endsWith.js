f178af9b52bb7d8920991f5c8c9365fc61f75bb4
Glenn Torrens <122960936+GutarManboy1@users.noreply.github.com>
2023-10-21 21:23:36 +0900

    Add files via upload

---
+++ CodingJS/WarmUP1/endsWith.js
@@ -0,0 +1,11 @@
+// Given a string, return true if it ends in "ly".
+
+// Examples
+
+// endsLy('oddly') → true
+// endsLy('y') → false
+// endsLy('oddl') → false
+
+function endsLy(str){
+    return str.endsWith('ly')? true : false;
+  }
