eb0cc3fa1edb7cac25f4f08d2d8bce466dc01be0
Glenn Torrens <122960936+GutarManboy1@users.noreply.github.com>
2023-10-22 21:41:08 +0900

    Add files via upload

---
+++ CodingJS/WarmUP1/twoAsOne.js
@@ -0,0 +1,15 @@
+// Given three ints, a b c, return true if it is possible to add two of the ints to get the third.
+
+// Examples
+
+// twoAsOne(1, 2, 3) → true
+// twoAsOne(3, 1, 2) → true
+// twoAsOne(3, 2, 2) → false
+
+function twoAsOne(a, b, c){
+    if ((a + b == c)||(a + c == b)|| (b + c== a)){
+    return true
+    }else{
+    return false
+    }
+  }
