d71ef1b9f948c0f15c700c706af920513e52fe6a
Glenn Torrens <122960936+GutarManboy1@users.noreply.github.com>
2023-10-31 14:21:56 +0900

    Add files via upload

--- CodingJS/WarmUP1/sameFirstLast.js
+++ CodingJS/WarmUP1/sameFirstLast.js
@@ -6,18 +6,15 @@
 // sameFirstLast([1, 2, 3, 1]) → true
 // sameFirstLast([1, 2, 1]) → true
 
-
 function sameFirstLast(nums){
-    if (nums.length >= 1){
-     return true 
-    }else if (nums[0] === nums[-1]){
-     return true
-    }else (nums == ['']){
-     return false
-    }
-    }
+    if (nums.length >= 1 && nums[0] == nums[nums.length-1]){
+    return true;
+    }else {
+     return false;
+    }   
+   }
 
-//couldnt solve this one, yet && still gonna push to github
+   //solved it! the the nums.length access the last number of the array and not the nums[-1] which would have been negative one! such an easy question if had known that!
 
 // Test → Expected	Yours	Result
 // sameFirstLast([1, 2, 3]) → false	true	✖	
