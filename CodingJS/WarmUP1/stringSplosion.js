95ef145bc0deb248cccf7fd210bed147797a1eea
Glenn Torrens <122960936+GutarManboy1@users.noreply.github.com>
2023-11-07 12:57:45 +0900

    Add files via upload

---
+++ CodingJS/WarmUP1/stringSplosion.js
@@ -0,0 +1,7 @@
+// Given a non-empty string like 'Code' return a string lik 'CCoCodCode'
+
+function stringSplosion(str){
+    return str.repeat(1, str.length-1)
+  }
+
+  //this is the answer i came up with and it accepts but doesn't provide test examples. 
