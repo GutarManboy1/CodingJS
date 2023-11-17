f674e5015af72c6d69cf7438a239fd49ac33e5a8
Glenn Torrens <122960936+GutarManboy1@users.noreply.github.com>
2023-11-17 14:02:15 +0900

    Add files via upload

---
+++ CodingJS/WarmUP1/lastTwo.js
@@ -0,0 +1,12 @@
+// Given a string of any length, return a new string where the last 2 chars, if present, are swapped, so "coding" yields "codign".
+
+// Examples
+
+// lastTwo('coding') → codign
+// lastTwo('cat') → cta
+// lastTwo('ab') → ba
+
+function lastTwo(str){
+  
+    return str.slice(0,str.length-2)+str.slice(-1)+str.slice(-2,-1)
+  }
