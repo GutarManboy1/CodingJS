7e581140c7131ed3450dbc5796be530f3a275a5b
Glenn Torrens <122960936+GutarManboy1@users.noreply.github.com>
2023-11-19 22:20:28 +0900

    Add files via upload

---
+++ CodingJS/WarmUP1/frontAgain.js
@@ -0,0 +1,21 @@
+// Given a string, return true if the first 2 chars in the string also appear at the end of the string, such as with "edited".
+
+// Examples
+
+// frontAgain('edited') → true
+// frontAgain('edit') → false
+// frontAgain('ed') → true
+
+function frontAgain(str){
+  
+    if(str.length <2){
+      return false
+       }
+    
+    if(str.substring(0,2)==str.substring(str.length-2)){
+      return true
+       }else{
+       return false
+       }
+   
+  }
