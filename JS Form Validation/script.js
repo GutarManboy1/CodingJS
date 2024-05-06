4f203bf2e7de112f5d4e05ab7bc8f730fc8a1895
Glenn Torrens <122960936+GutarManboy1@users.noreply.github.com>
2024-05-06 18:22:17 +0900

    Add files via upload

---
+++ JS Form Validation/script.js
@@ -0,0 +1,81 @@
+const nameError = document.getElementById("name-error");
+const phoneError = document.getElementById("phone-error");
+const emailError = document.getElementById("email-error");
+const messageError = document.getElementById("message-error");
+const submitError = document.getElementById("submit-error");
+
+function validateName() {
+  let name = document.getElementById("contact-name").value;
+
+  if (name.length == 0) {
+    nameError.innerHTML = 'Name is Required!'
+    return false;
+  }
+  //here the if the name doesnt match
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+    nameError.innerHTML = 'Write full name!!';
+    return false;
+  }
+  nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validatePhone() {
+  let phone = document.getElementById("contact-phone").value;
+
+  if (phone.length == 0) {
+    phoneError.innerHTML = 'Number is Required!';
+    return false;
+  }
+  if (phone.length !== 11) {
+    phoneError.innerHTML = 'Number should be 11 digits';
+    return false;
+  }
+  //same as above
+  if (!phone.match(/^[0-9]{11}$/)) {
+    phoneError.innerHTML = 'Only digits please!'
+    return false;
+  }
+  phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+function validateEmail() {
+  let email = document.getElementById("contact-email").value;
+
+  if (email.length == 0) {
+    emailError.innerHTML = 'Email is Required!';
+    return false;
+  }
+  //same as above
+  if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,6}$/)) {
+    emailError.innerHTML = 'Email Invalid!';
+    return false;
+  }
+  emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validateMessage() {
+  let message = document.getElementById("contact-message").value;
+
+  let required = 30;
+  let left = required - message.length;
+
+  if (left > 0) {
+    messageError.innerHTML = left + 'more characters required';
+    return false;
+  }
+
+  messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+//this last function validates all of the inputs. if they are not all valid it will flash the error message in the timeout
+// for 3 seconds and disappear. display block makes sure that the message is visible  after becoming none in 3 seconds
+function validateForm() {
+  if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
+    submitError.style.display = 'block';
+    submitError.innerHTML = 'Please fix error, bruh!';
+    setTimeout(function () { submitError.style.display = 'none'; }, 3000);
+    return false;
+  }
+}
